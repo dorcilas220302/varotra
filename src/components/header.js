db16947e30dab5c6876cb740dc72708644b9da64
dorcilas220306 <validordorcilas@gmail.com>
2025-05-22 10:02:34 -0700

    change some style

--- src/components/header.js
+++ src/components/header.js
@@ -10,9 +10,16 @@ const SuziLinge=()=>{
                 <h1>suzi lingerie</h1>
             </div>
             <div className="contact">
+              <a href="https://web.facebook.com/profile.php?id=100092459257512&sk=about">   
                 <i className="fab fa-facebook-f"></i>
+              </a>
+              <a href="
+                 https://www.instagram.com/suzi03lingerie?igsh=MnFreXFycmZoYWJt">
                 <i className="fab fa-instagram"></i>
+              </a>
+              <a href="https://web.facebook.com/profile.php?id=100092459257512&sk=about">
                 <i className="fab fa-whatsapp"></i>
+              </a>
             </div>
           </header>
           <section className="section-1">
